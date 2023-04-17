a02856183766f1b13c90e9800a3a193f990d4ddc
igorlech <eaaigle@students.eaaa.dk>
2023-04-17 19:49:47 +0200

    added onmouse events to track cursor

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -10,5 +10,40 @@ class SketchPad {
     container.appendChild(this.canvas);
 
     this.ctx = this.canvas.getContext("2d");
+
+    this.path = [];
+    this.isDrawing = false;
+
+    this.#addEventListeners();
+  }
+
+  #addEventListeners() {
+    this.canvas.onmousedown = (e) => {
+      const rect = this.canvas.getBoundingClientRect();
+      // GETTING MOUSE POSITION
+      const mouse = [
+        Math.round(e.clientX - rect.left),
+        Math.round(e.clientY - rect.top),
+      ];
+      // PATH LOGIC
+      this.path = [mouse];
+      this.isDrawing = true;
+    };
+
+    this.canvas.onmousemove = (e) => {
+      if (this.isDrawing) {
+        const rect = this.canvas.getBoundingClientRect();
+        const mouse = [
+          Math.round(e.clientX - rect.left),
+          Math.round(e.clientY - rect.top),
+        ];
+        this.path.push(mouse);
+        console.log(this.path.length);
+      }
+    };
+
+    this.canvas.onmouseup = () => {
+      this.isDrawing = false;
+    };
   }
 }
